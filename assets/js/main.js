f3e3e801f548c0aa478b0b87e3c2b3176655aded
Nigel Payne <noproblemnigel@gmail.com>
2019-10-31 14:01:32 +0000

    about page updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
 var options = {
     // vertical offset in px before element is first unpinned
-    offset : 0,
+    offset : 60,
     // or you can specify tolerance individually for up/down scroll
     tolerance : {
         up : 0,
