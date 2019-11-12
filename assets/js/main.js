0e7d14378b0bb2de38bed425b65ddd43d0ac0c7d
Nigel Payne <noproblemnigel@gmail.com>
2019-11-12 00:14:59 +0000

    mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,15 @@ function openCity(evt, cityName) {
 }
 
 document.getElementById("defaultOpen").click()
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
