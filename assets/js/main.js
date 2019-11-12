983742c4059b6e15b40513a760deded28471b608
Nigel Payne <noproblemnigel@gmail.com>
2019-11-12 21:59:47 +0000

    add responsive nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
+function isNavVisible(nav) {
+  return ( nav.classList.contains('responsive') ? true : false );
+}
+
 // grab an element
 var myElement = document.querySelector("header");
+
+var nav = document.getElementById('myTopnav');
+
 // construct an instance of Headroom, passing the element
 var options = {
     // vertical offset in px before element is first unpinned
@@ -8,7 +15,18 @@ var options = {
     tolerance : {
         up : 10,
         down : 0
+    },
+    onUnpin: function() {
+      if ( isNavVisible(nav) ) {
+        this.elem.classList.remove(this.classes.unpinned);
+        this.elem.classList.add(this.classes.pinned);
+      }
+      else {
+        this.elem.classList.add(this.classes.unpinned);
+        this.elem.classList.remove(this.classes.pinned);
+      }
     }
+
 
 };
 
@@ -40,18 +58,16 @@ function openCity(evt, cityName) {
 
 document.getElementById("defaultOpen").click()
 
-
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   var toggleTag = document.getElementById("toggleTag");
   if (x.className === "topnav") {
     x.className += " responsive";
-    toggleTag.innerHTML = "Close";
+    toggleTag.innerHTML = '<img src="/assets/img/close.svg">';
   } else {
     x.className = "topnav";
-    toggleTag.innerHTML = "Menu";
+    toggleTag.innerHTML = '<img src="/assets/img/menu.svg">';
   }
 
 }
